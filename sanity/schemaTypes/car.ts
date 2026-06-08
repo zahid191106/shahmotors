e9ae19e6639109705fd93b31a50c7051d93f4d17
Muhammad Zahid <muhammadzahid191106@gmail.com>
2026-06-09 00:59:42 +0500

    changing

--- sanity/schemaTypes/car.ts
+++ sanity/schemaTypes/car.ts
@@ -59,7 +59,7 @@ export const car = defineType({
     }),
     defineField({
       name: 'price',
-      title: 'Price (USD)',
+      title: 'Price (Euro)',
       type: 'number',
       validation: (Rule) => Rule.required().positive(),
     }),
